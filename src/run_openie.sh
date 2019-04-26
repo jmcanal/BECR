9ddcff067dd47d1c623168cd5ceefd51694b6a8d
Zoe Winkworth <zoew2@washington.edu>
2019-04-25 19:18:19 -0700

    setting up openie

--- src/run_openie.sh
+++ src/run_openie.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-java -Xmx10g -XX:+UseConcMarkSweepGC -jar ../lib/openie-assembly.jar  ../data/open_ie_test.txt
-../outputs/test-openie.op
+
+java -Xmx10g -XX:+UseConcMarkSweepGC -jar ../lib/openie/openie-assembly.jar ../data/open_ie-test.txt ../outputs/eng5dev-openie.op
+
+#java -Xmx10g -XX:+UseConcMarkSweepGC -jar ../lib/openie/openie-assembly.jar ../data/5_pt/eng/twitter-2016dev-CE-sents.txt ../outputs/eng5dev-openie.op
