13ec36062bca6855309df18004ee9aed783be705
Zoe Winkworth <zoew2@washington.edu>
2019-05-24 02:56:42 -0700

    filtering etc

--- src/split_data.py
+++ src/split_data.py
@@ -1,5 +1,8 @@
 import sys
+import os.path
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from sklearn.model_selection import train_test_split
+from lib.tweet_tagger import CMUTweetTagger
 
 
 def main():
@@ -8,10 +11,11 @@ def main():
     :return:
     """
     data_file = sys.argv[1]
-    train_file = sys.argv[2]
-    dev_file = sys.argv[3]
-    devtest_file = sys.argv[4]
-    test_file = sys.argv[5]
+    train_tok = sys.argv[2]
+    train_file = sys.argv[3]
+    dev_file = sys.argv[4]
+    devtest_file = sys.argv[5]
+    test_file = sys.argv[6]
 
     data = [line.strip("\n") for line in open(data_file, "r")]
 
@@ -21,6 +25,16 @@ def main():
 
     with open(train_file, 'w') as train:
         print("\n".join(x_train), file=train)
+
+    tag_list = CMUTweetTagger.runtagger_parse(x_train)
+    with open(train_tok, 'a+') as tok:
+        for tags in tag_list:
+            tagged_toks = []
+            for tuple in tags:
+                word = tuple[0].encode('utf-8').decode('latin1')
+                tag = tuple[1].encode('utf-8').decode('latin1')
+                tagged_toks.append("{}:{}".format(word, tag))
+            print(" ".join(tagged_toks), file=tok)
 
     with open(dev_file, 'w') as dev:
         print("\n".join(x_dev), file=dev)
