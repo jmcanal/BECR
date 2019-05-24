cc8738ca181bdcaf26c3777492f2951983de8179
Zoe Winkworth <zoew2@washington.edu>
2019-05-24 02:56:42 -0700

    filtering etc

--- src/build_dict.sh
+++ src/build_dict.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-lexicon_file='../lib/emotion_lexicon/DepecheMood/DepecheMood_normfreq.txt'
+#dm_lexicon='../lib/emotion_lexicon/DepecheMood/DepecheMood_freq.txt'
+dm_lexicon='../lib/emotion_lexicon/DepecheMood/DepecheMood_normfreq.txt'
+nrc_lexicon='../lib/emotion_lexicon/NRC/NRC-Emotion-Lexicon-Wordlevel-v0.92.txt'
+kw_lexicon='../lib/emotion_lexicon/emotion_kw_list/emotion_keywords.txt'
 
-python3 build_emotion_dictionary.py ${lexicon_file}
+python3 build_lexicon.py ${dm_lexicon} 'dm'
+python3 build_lexicon.py ${nrc_lexicon} 'nrc'
+python3 build_lexicon.py ${kw_lexicon} 'kw'
+python3 build_lexicon.py ${kw_lexicon} 'kwsyns'
